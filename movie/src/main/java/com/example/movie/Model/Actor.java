65dd471c10cbc8e7894bf145295cbe36196798e7
Kamil Markowski <33059975+gugol9@users.noreply.github.com>
2023-05-19 23:16:02 +0200

    1.1 all ok deleteUserByID

--- movie/src/main/java/com/example/movie/Model/Actor.java
+++ movie/src/main/java/com/example/movie/Model/Actor.java
@@ -4,50 +4,29 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 @Getter
 @Setter
+@ToString
 @Entity
-
 @Table(name = "actor")
 public class Actor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ActorID")
-    private Long actorid;
+    private Long ActorID;
 
-    @Column(name = "firstname")
     private String firstname;
-
-    @Column(name = "lastname")
     private String lastname;
-
-    @Column(name = "birth")
     private LocalDate birth;
-
-    @Column(name = "gender")
     private String gender;
-
-    @Column(name = "score")
     private int score;
-
-    @Column(name = "oscar")
     private boolean oscar;
 
 
-
-    @OneToMany(mappedBy = "actor", cascade = CascadeType.ALL)
-    private List<Film> films;
-
-
-
-
-
-
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ActorID", updatable = false, insertable = false)
+    @ToString.Exclude
+    private List<Film> filmList;
 }
