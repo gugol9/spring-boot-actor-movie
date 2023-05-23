060ccac9701d246686d15a78c4b9ce1eeeae6180
Kamil Markowski <markian99@wp.pl>
2023-05-23 17:36:40 +0200

    fix

---
+++ src/main/java/com/movie/springbootmovie/SpringBootMovieApplication.java
@@ -0,0 +1,13 @@
+package com.movie.springbootmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMovieApplication.class, args);
+	}
+
+}
--- src/test/java/com/movie/springbootmovie/SpringBootMovieApplicationTests.java
+++ src/test/java/com/movie/springbootmovie/SpringBootMovieApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.movie;
+package com.movie.springbootmovie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MovieApplicationTests {
+class SpringBootMovieApplicationTests {
 
 	@Test
 	void contextLoads() {
